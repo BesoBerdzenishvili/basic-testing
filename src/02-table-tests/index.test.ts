90213bdcd0488575b683ae32eb2d8bb93a120e12
fliderman01 <fliderman01@gmail.com>
2025-11-07 19:07:43 +0400

    test: 2 - table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,18 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 4, b: 2, action: Action.Add, expected: 6 },
+  { a: 5, b: 2, action: Action.Add, expected: 7 },
+  { a: 6, b: 2, action: Action.Add, expected: 8 },
+];
 
-describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+describe.each(testCases)('.add(%i, %i)', (param) => {
+  test(`Should ${param.action} ${param.a} to ${param.b} and return ${param.expected}`, () => {
+    expect(
+      simpleCalculator({ a: param.a, b: param.b, action: param.action }),
+    ).toBe(param.expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
